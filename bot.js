16d8cd8995370fe5d2ec865c0dad40e8aa8b6644
Valentin Abramenkov <valentin.abramenkov@kesko.fi>
2016-09-04 15:55:13 +0300

    Added project base

--- bot.js
+++ bot.js
@@ -1,31 +1,27 @@
-/////////////////////////////////////////
-// Safety: Uncomment everything to use //
-/////////////////////////////////////////
+// dependencies
+var _ = require('lomath');
 
-// // dependencies
-// var _ = require('lomath');
+// API as superclass that bot inherits methods from
+var API = require(__dirname + '/API.js')
 
-// // API as superclass that bot inherits methods from
-// var API = require(__dirname + '/API.js')
+// The bot object prototype
+// bot extends and inherits methods of API
+var bot = function(token, webhookUrl) {
+    API.apply(this, arguments);
+    // set webhook on construction: override the old webhook
+    this.setWebhook(webhookUrl || '');
 
-// // The bot object prototype
-// // bot extends and inherits methods of API
-// var bot = function(token, webhookUrl) {
-//     API.apply(this, arguments);
-//     // set webhook on construction: override the old webhook
-//     this.setWebhook(webhookUrl || '');
+}
 
-// }
-
-// // set prototype to API
-// bot.prototype = API.prototype;
-// // set constructor back to bot
-// bot.prototype.constructor = bot;
+// set prototype to API
+bot.prototype = API.prototype;
+// set constructor back to bot
+bot.prototype.constructor = bot;
 
 
 /**
  * Handles a Telegram Update object sent from the server. Extend this method for your bot.
- * 
+ *
  * @category Bot
  * @param {Object} req The incoming HTTP request.
  * @param {Object} res The HTTP response in return.
@@ -40,30 +36,30 @@
  *     bot1.handle(req, res)
  * })
  * // Then bot will handle the incoming Update from you, routed from Telegram!
- * 
+ *
  */
-// bot.prototype.handle = function(req, res) {
-//     // the Telegram Update object. Useful shits
-//     var Update = req.body,
-//         // the telegram Message object
-//         Message = Update.message,
-//         // the user who sent it
-//         user_id = Message.from.id,
-//         // id of the chat(room)
-//         chat_id = Message.chat.id;
+bot.prototype.handle = function(req, res) {
+    // the Telegram Update object. Useful shits
+    var Update = req.body,
+        // the telegram Message object
+        Message = Update.message,
+        // the user who sent it
+        user_id = Message.from.id,
+        // id of the chat(room)
+        chat_id = Message.chat.id;
 
 //     ////////////////////////
 //     // Extend from here:  //
 //     ////////////////////////
 //     // you may call the methods from API.js, which are all inherited by this bot class
-    
-//     // echo
-//     this.sendMessage(chat_id, "you said: " + Message.text);
 
-// }
+    // echo
+    this.sendMessage(chat_id, "you said: " + Message.text);
+
+}
 
 // export the bot class
-// module.exports = bot;
+module.exports = bot;
 
 // sample keyboard
 // var kb = {
--- index.js
+++ index.js
@@ -1,59 +1,55 @@
-/////////////////////////////////////////
-// Safety: Uncomment everything to use //
-/////////////////////////////////////////
+// Dependencies
+var _ = require('lomath');
+var express = require('express');
+var app = express();
 
-// // dependencies
-// // express
-// var _ = require('lomath');
-// var express = require('express');
-// var app = express();
-// // express middlewares
-// var morgan = require('morgan');
-// var ejs = require('ejs');
-// var bodyParser = require('body-parser');
-// var multer = require('multer');
+// Express middlewares
+var morgan = require('morgan');
+var ejs = require('ejs');
+var bodyParser = require('body-parser');
+var multer = require('multer');
 
-// // telegram bot
-// var bot = require(__dirname + '/bot.js');
-// var token = 'your example token';
-// var webhookUrl = 'your webhook url'
-// var bot1 = new bot(process.env.TOKEN || token, process.env.WEBHOOK || webhookUrl);
+// Init Telegram bot
+var bot = require(__dirname + '/bot.js');
+var fleimiBot = new bot(process.env.TOKEN, process.env.WEBHOOK);
 
+// HTML render engine
+app.engine('.html', ejs.__express);
+app.set('view engine', 'html');
 
-// // engine to render HTML
-// app.engine('.html', ejs.__express);
-// app.set('view engine', 'html');
-// // set the port number
-// app.set('port', process.env.PORT || 8443);
+// Set port
+app.set('port', process.env.PORT || 8443);
 
-// // Mount middlewares defaulted for root: 
-// // log all HTTP calls for debugging
-// // app.use(morgan('combined'));
-// // use resources for html: HTML, JS and CSS etc.
-// app.use(express.static(__dirname + '/views'));
-// // parse incoming formData into JSON
-// app.use(bodyParser.json());
-// app.use(multer());
+// Mount middlewares defaulted for root:
+// Log all HTTP calls for debugging
+// app.use(morgan('combined'));
+
+// Use resources for html: HTML, JS and CSS etc.
+app.use(express.static(__dirname + '/views'));
+
+// Parse incoming formData into JSON
+app.use(bodyParser.json());
+app.use(multer());
 
 
-// // route: concise way to group all HTTP methods for a path
-// app.route('/')
-//     .get(function(req, res) {
-//         // console.log("you GET")
-//         res.render('index')
-//     })
-//     .post(function(req, res) {
-//         // send back to end req-res cycle
-//         res.json('okay, received\n');
-//         // robot handle as middleware for POST
-//         bot1.handle(req, res)
-//     })
-//     .put(function(req, res) {
-//         res.send("you just called PUT\n")
-//     })
+// Routes
+app.route('/')
+    .get(function(req, res) {
+        // console.log("you GET")
+        res.render('index')
+    })
+    .post(function(req, res) {
+        // send back to end req-res cycle
+        res.json('okay, received\n');
+        // robot handle as middleware for POST
+        fleimiBot.handle(req, res)
+    })
+    .put(function(req, res) {
+        res.send("you just called PUT\n")
+    })
 
 
-// // finally, listen to the specific port for any calls
-// app.listen(app.get('port'), function() {
-//     console.log('Node app is running on port', app.get('port'));
-// });
+// Listen to the port
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+});
